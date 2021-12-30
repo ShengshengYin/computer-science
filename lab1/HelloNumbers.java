188ef4e89c5728aab27f8b23fb8ae788329daa48
ShengshengYin <157791105@qq.com>
2021-12-30 08:08:54 +0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,20 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int x = 0;
+		int sum = 0;
+
+		while(x<10){
+		    sum = sum + x; 	
+		    x++;
+		    System.out.print(sum + " ");
+		}
+               
+		System.out.println(5 + "horse");
+		System.out.println(5 + "10");
+		System.out.println(5 + 10);
+
+	}
+
+
+
+}
